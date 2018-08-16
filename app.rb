f6f7aa5cc570ac49ee52e4c79f85e5663c9978a6
Christine Xiang <yutingcxiang@gmail.com>
2018-08-16 19:17:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Christine"
+  end
+  
+  get '/hometown' do
+    "My hometown is NYC"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Fifteen"
+  end
+  
 end
